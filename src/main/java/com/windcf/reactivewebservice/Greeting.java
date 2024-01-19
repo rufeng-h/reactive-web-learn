2e2ceb427fda9fd4cbd368777790d95c7522b7f0
rufeng-h <chunfengh163@163.com>
2024-01-19 17:18:06 +0800

    first commit

---
+++ src/main/java/com/windcf/reactivewebservice/Greeting.java
@@ -0,0 +1,4 @@
+package com.windcf.reactivewebservice;
+
+public record Greeting(String message) {
+}
---
+++ src/main/java/com/windcf/reactivewebservice/GreetingHandler.java
@@ -0,0 +1,20 @@
+package com.windcf.reactivewebservice;
+
+import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler implements HandlerFunction<ServerResponse> {
+    @NonNull
+    public Mono<ServerResponse> handle(@NonNull ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(new Greeting("hello")));
+    }
+
+}
